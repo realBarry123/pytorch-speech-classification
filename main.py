ef9bf1b8207edc4efecdfa238287919c0acc1c67
realBarry123 <barryyu28@gmail.com>
2024-01-03 17:24:35 -0800

    Create M5 class and model.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ import matplotlib.pyplot as plt  # data visualization
 import IPython.display as ipd
 from tqdm import tqdm
 
+from model import M5
+
 # import data
 
 from torchaudio.datasets import SPEECHCOMMANDS
@@ -96,3 +98,67 @@ index = label_to_index(word_start)
 word_recovered = index_to_label(index)
 
 print(word_start, "-->", index, "-->", word_recovered)
+
+
+def pad_sequence(batch):
+    # Make all tensor in a batch the same length by padding with zeros
+    batch = [item.t() for item in batch]
+    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
+    return batch.permute(0, 2, 1)
+
+
+def collate_fn(batch):
+
+    # A data tuple has the form:
+    # waveform, sample_rate, label, speaker_id, utterance_number
+
+    tensors, targets = [], []
+
+    # Gather in lists, and encode labels as indices
+    for waveform, _, label, *_ in batch:
+        tensors += [waveform]
+        targets += [label_to_index(label)]
+
+    # Group the list of tensors into a batched tensor
+    tensors = pad_sequence(tensors)
+    targets = torch.stack(targets)
+
+    return tensors, targets
+
+
+batch_size = 256
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if DEVICE == "cuda":
+    num_workers = 1
+    pin_memory = True
+else:
+    num_workers = 0
+    pin_memory = False
+
+train_loader = torch.utils.data.DataLoader(
+    train_set,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=collate_fn,
+    num_workers=num_workers,
+    pin_memory=pin_memory,
+)
+test_loader = torch.utils.data.DataLoader(
+    test_set,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    collate_fn=collate_fn,
+    num_workers=num_workers,
+    pin_memory=pin_memory,
+)
+
+model = M5(n_input=transformed.shape[0], n_output=len(LABELS))
+model.to(DEVICE)
+print(model)
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+n = count_parameters(model)
+print("Number of parameters: %s" % n)
